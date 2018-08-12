c545f09217c13610938272434a76ed84badf733b
Josiah Maharaj <josiahmaharaj@outlook.com>
2018-08-12 01:57:22 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return <h1>Trinidad Bible Relay</h1>;
+  }
+}
+
+export default App;
